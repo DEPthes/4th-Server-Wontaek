9d57f97f8e2490952fd46a9222e162f3933abfc3
taek <wontaek88@gmail.com>
2025-04-03 22:20:14 +0900

    Feat: 프로젝트 생성

---
+++ servlet/src/test/java/taek/servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package taek.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
